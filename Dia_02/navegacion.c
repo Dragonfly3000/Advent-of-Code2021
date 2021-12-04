66b99bc821c0a8399b0e7ada2b9934e199aa4459
Jesus Manuel <jesusmanueldl@gmail.com>
2021-12-03 20:50:56 -0600

    segunda parte resuelta

--- Dia_02/navegacion.c
+++ Dia_02/navegacion.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
+
+	FILE *archivo;
+	char cad[20]={0};
+	int d = 0;
+	int f = 0;
+    int tam = 0;
+	archivo = fopen("input.txt","r");
+	if (archivo == NULL)
+    {
+        printf("\nError de apertura del archivo. \n\n");
+        exit(1);
+    }
+    else
+    {
+ 	    while (!feof(archivo))
+ 	    {
+ 	        fgets(cad,20,archivo);
+            tam = strlen(cad);
+            if(cad[0] == 'f'){
+                f += cad[tam-2] - '0';
+            }
+            else if(cad[0] == 'd'){
+                d += cad[tam-2] - '0';
+            }
+            else if(cad[0] == 'u'){
+                d -= cad[tam-2] - '0';
+            }
+        }
+    }
+    fclose(archivo);
+    printf("%d\n",f);
+    printf("%d",d);
+
+
 
 
 return 0;
